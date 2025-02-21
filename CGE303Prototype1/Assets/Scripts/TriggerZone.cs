6451f138a45afe34e46dd1a52afa2a181e899ac5
gkobe11 <gavinkobe0@gmail.com>
2025-02-21 14:44:48 -0600

    Follow along continued

--- CGE303Prototype1/Assets/Scripts/TriggerZone.cs
+++ CGE303Prototype1/Assets/Scripts/TriggerZone.cs
@@ -1,12 +1,26 @@
 using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
+//text mesh pro
+using TMPro;
 
 public class TriggerZone : MonoBehaviour
 {
+    //set to reference in inspector
+    public TMP_Text output;
+
+    //enter text to display in inspector
+    public string textToDisplay;
+
     private void OnTriggerEnter2D(Collider2D collision)
     {
-        Debug.Log("Triggered by: " + collision.gameObject.name);
+        //set player tag on player in inspector
+        if (collision.gameObject.tag == "Player")
+        {
+            //display "You Win!" on screen
+            output.text = textToDisplay;
+        }
+        //Debug.Log("Triggered by: " + collision.gameObject.name);
     }
 
     // Update is called once per frame
